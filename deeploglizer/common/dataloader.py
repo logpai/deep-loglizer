ec38d0ea98faec37c810df449ff7117a3a43993c
ase21-843 <83080529+ase21-843@users.noreply.github.com>
2021-05-11 15:07:30 +0800

    update

--- deeploglizer/common/dataloader.py
+++ deeploglizer/common/dataloader.py
@@ -171,8 +171,9 @@ def load_HDFS_id(log_id_path):
             break
         sample = {"templates": line.split(), "label": 0}
         session_test[idx] = sample
+    print(len(session_test))
 
-    for idx, line in enumerate(open(test_anomaly)):
+    for idx, line in enumerate(open(test_anomaly), len(session_test)):
         if idx > 50000:
             break
         sample = {"templates": line.split(), "label": 1}
