e91e55f263828ba13b5fa1a30694edbd456e28ac
ase21-843 <83080529+ase21-843@users.noreply.github.com>
2021-05-12 01:03:46 +0800

    update

--- demo/runs/HDFS/deeplog_anomaly.sh
+++ demo/runs/HDFS/deeplog_anomaly.sh
@@ -1,21 +1,21 @@
-python deeplog_demo.py --test_ratio 0.8 --train_anomaly_ratio 0 -- feature_type sequentials --dataset HDFS --label_type anomaly --gpu 1 > logs/deeplog.0_anomaly
+python deeplog_demo.py --test_ratio 0.8 --train_anomaly_ratio 0 --feature_type sequentials --dataset HDFS --label_type anomaly --gpu 1 > logs/deeplog.0_anomaly
 
-python deeplog_demo.py --test_ratio 0.8 --train_anomaly_ratio 0.1 -- feature_type sequentials --dataset HDFS --label_type anomaly --gpu 1 > logs/deeplog.1_anomaly
+python deeplog_demo.py --test_ratio 0.8 --train_anomaly_ratio 0.1 --feature_type sequentials --dataset HDFS --label_type anomaly --gpu 1 > logs/deeplog.1_anomaly
 
-python deeplog_demo.py --test_ratio 0.8 --train_anomaly_ratio 0.2 -- feature_type sequentials --dataset HDFS --label_type anomaly --gpu 1 > logs/deeplog.2_anomaly
+python deeplog_demo.py --test_ratio 0.8 --train_anomaly_ratio 0.2 --feature_type sequentials --dataset HDFS --label_type anomaly --gpu 1 > logs/deeplog.2_anomaly
 
-python deeplog_demo.py --test_ratio 0.8 --train_anomaly_ratio 0.3 -- feature_type sequentials --dataset HDFS --label_type anomaly --gpu 1 > logs/deeplog.3_anomaly
+python deeplog_demo.py --test_ratio 0.8 --train_anomaly_ratio 0.3 --feature_type sequentials --dataset HDFS --label_type anomaly --gpu 1 > logs/deeplog.3_anomaly
 
-python deeplog_demo.py --test_ratio 0.8 --train_anomaly_ratio 0.4 -- feature_type sequentials --dataset HDFS --label_type anomaly --gpu 1 > logs/deeplog.4_anomaly
+python deeplog_demo.py --test_ratio 0.8 --train_anomaly_ratio 0.4 --feature_type sequentials --dataset HDFS --label_type anomaly --gpu 1 > logs/deeplog.4_anomaly
 
-python deeplog_demo.py --test_ratio 0.8 --train_anomaly_ratio 0.5 -- feature_type sequentials --dataset HDFS --label_type anomaly --gpu 1 > logs/deeplog.5_anomaly
+python deeplog_demo.py --test_ratio 0.8 --train_anomaly_ratio 0.5 --feature_type sequentials --dataset HDFS --label_type anomaly --gpu 1 > logs/deeplog.5_anomaly
 
-python deeplog_demo.py --test_ratio 0.8 --train_anomaly_ratio 0.6 -- feature_type sequentials --dataset HDFS --label_type anomaly --gpu 1 > logs/deeplog.6_anomaly
+python deeplog_demo.py --test_ratio 0.8 --train_anomaly_ratio 0.6 --feature_type sequentials --dataset HDFS --label_type anomaly --gpu 1 > logs/deeplog.6_anomaly
 
-python deeplog_demo.py --test_ratio 0.8 --train_anomaly_ratio 0.7 -- feature_type sequentials --dataset HDFS --label_type anomaly --gpu 1 > logs/deeplog.7_anomaly
+python deeplog_demo.py --test_ratio 0.8 --train_anomaly_ratio 0.7 --feature_type sequentials --dataset HDFS --label_type anomaly --gpu 1 > logs/deeplog.7_anomaly
 
-python deeplog_demo.py --test_ratio 0.8 --train_anomaly_ratio 0.8 -- feature_type sequentials --dataset HDFS --label_type anomaly --gpu 1 > logs/deeplog.8_anomaly
+python deeplog_demo.py --test_ratio 0.8 --train_anomaly_ratio 0.8 --feature_type sequentials --dataset HDFS --label_type anomaly --gpu 1 > logs/deeplog.8_anomaly
 
-python deeplog_demo.py --test_ratio 0.8 --train_anomaly_ratio 0.9 -- feature_type sequentials --dataset HDFS --label_type anomaly --gpu 1 > logs/deeplog.9_anomaly
+python deeplog_demo.py --test_ratio 0.8 --train_anomaly_ratio 0.9 --feature_type sequentials --dataset HDFS --label_type anomaly --gpu 1 > logs/deeplog.9_anomaly
 
-python deeplog_demo.py --test_ratio 0.8 --train_anomaly_ratio 1 -- feature_type sequentials --dataset HDFS --label_type anomaly --gpu 1 > logs/deeplog.10_anomaly
+python deeplog_demo.py --test_ratio 0.8 --train_anomaly_ratio 1 --feature_type sequentials --dataset HDFS --label_type anomaly --gpu 1 > logs/deeplog.10_anomaly
--- demo/runs/HDFS/deeplog_next_log.sh
+++ demo/runs/HDFS/deeplog_next_log.sh
@@ -1,21 +1,21 @@
-python deeplog_demo.py --test_ratio 0.8 --train_anomaly_ratio 0 -- feature_type sequentials --dataset HDFS --label_type next_log --gpu 0 > logs/deeplog.0 2>&1
+python deeplog_demo.py --test_ratio 0.8 --train_anomaly_ratio 0 --feature_type sequentials --dataset HDFS --label_type next_log --gpu 0 > logs/deeplog.0
 
-python deeplog_demo.py --test_ratio 0.8 --train_anomaly_ratio 0.1 -- feature_type sequentials --dataset HDFS --label_type next_log --gpu 0 > logs/deeplog.1 2>&1
+python deeplog_demo.py --test_ratio 0.8 --train_anomaly_ratio 0.1 --feature_type sequentials --dataset HDFS --label_type next_log --gpu 0 > logs/deeplog.1
 
-python deeplog_demo.py --test_ratio 0.8 --train_anomaly_ratio 0.2 -- feature_type sequentials --dataset HDFS --label_type next_log --gpu 0 > logs/deeplog.2 2>&1
+python deeplog_demo.py --test_ratio 0.8 --train_anomaly_ratio 0.2 --feature_type sequentials --dataset HDFS --label_type next_log --gpu 0 > logs/deeplog.2
 
-python deeplog_demo.py --test_ratio 0.8 --train_anomaly_ratio 0.3 -- feature_type sequentials --dataset HDFS --label_type next_log --gpu 0 > logs/deeplog.3 2>&1
+python deeplog_demo.py --test_ratio 0.8 --train_anomaly_ratio 0.3 --feature_type sequentials --dataset HDFS --label_type next_log --gpu 0 > logs/deeplog.3
 
-python deeplog_demo.py --test_ratio 0.8 --train_anomaly_ratio 0.4 -- feature_type sequentials --dataset HDFS --label_type next_log --gpu 0 > logs/deeplog.4 2>&1
+python deeplog_demo.py --test_ratio 0.8 --train_anomaly_ratio 0.4 --feature_type sequentials --dataset HDFS --label_type next_log --gpu 0 > logs/deeplog.4
 
-python deeplog_demo.py --test_ratio 0.8 --train_anomaly_ratio 0.5 -- feature_type sequentials --dataset HDFS --label_type next_log --gpu 0 > logs/deeplog.5 2>&1
+python deeplog_demo.py --test_ratio 0.8 --train_anomaly_ratio 0.5 --feature_type sequentials --dataset HDFS --label_type next_log --gpu 0 > logs/deeplog.5
 
-python deeplog_demo.py --test_ratio 0.8 --train_anomaly_ratio 0.6 -- feature_type sequentials --dataset HDFS --label_type next_log --gpu 0 > logs/deeplog.6 2>&1
+python deeplog_demo.py --test_ratio 0.8 --train_anomaly_ratio 0.6 --feature_type sequentials --dataset HDFS --label_type next_log --gpu 0 > logs/deeplog.6
 
-python deeplog_demo.py --test_ratio 0.8 --train_anomaly_ratio 0.7 -- feature_type sequentials --dataset HDFS --label_type next_log --gpu 0 > logs/deeplog.7 2>&1
+python deeplog_demo.py --test_ratio 0.8 --train_anomaly_ratio 0.7 --feature_type sequentials --dataset HDFS --label_type next_log --gpu 0 > logs/deeplog.7
 
-python deeplog_demo.py --test_ratio 0.8 --train_anomaly_ratio 0.8 -- feature_type sequentials --dataset HDFS --label_type next_log --gpu 0 > logs/deeplog.8 2>&1
+python deeplog_demo.py --test_ratio 0.8 --train_anomaly_ratio 0.8 --feature_type sequentials --dataset HDFS --label_type next_log --gpu 0 > logs/deeplog.8
 
-python deeplog_demo.py --test_ratio 0.8 --train_anomaly_ratio 0.9 -- feature_type sequentials --dataset HDFS --label_type next_log --gpu 0 > logs/deeplog.9 2>&1
+python deeplog_demo.py --test_ratio 0.8 --train_anomaly_ratio 0.9 --feature_type sequentials --dataset HDFS --label_type next_log --gpu 0 > logs/deeplog.9
 
-python deeplog_demo.py --test_ratio 0.8 --train_anomaly_ratio 1 -- feature_type sequentials --dataset HDFS --label_type next_log --gpu 0 > logs/deeplog.10 2>&1
+python deeplog_demo.py --test_ratio 0.8 --train_anomaly_ratio 1 --feature_type sequentials --dataset HDFS --label_type next_log --gpu 0 > logs/deeplog.10
