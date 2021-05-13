6b89946801a4f237f4198db891d8a172962b2a01
ase21-843 <83080529+ase21-843@users.noreply.github.com>
2021-05-13 12:48:38 +0800

    update

--- demo/runs/HDFS/sequential.sh
+++ demo/runs/HDFS/sequential.sh
@@ -6,3 +6,21 @@ python ae_demo.py --dataset HDFS --feature_type sequentials --train_anomaly_rati
 # supervised
 python deeplog_demo.py --dataset HDFS --feature_type sequentials --train_anomaly_ratio 1 --label_type anomaly --gpu 3 B
 python cnn_demo.py --dataset HDFS --feature_type sequentials --train_anomaly_ratio 1 --label_type anomaly --gpu 3 B
+
+
+
+
+
+--embedding_dim 8 --hidden_size 100
+--embedding_dim 8 --hidden_size 200
+--embedding_dim 8 --hidden_size 300
+--embedding_dim 8 --hidden_size 400
+--embedding_dim 8 --hidden_size 500
+
+
+python deeplog_demo.py --dataset HDFS --feature_type sequentials --train_anomaly_ratio 0 --label_type next_log --gpu 0 --embedding_dim 8 --hidden_size 32 B
+python deeplog_demo.py --dataset HDFS --feature_type sequentials --train_anomaly_ratio 0 --label_type next_log --gpu 0 --embedding_dim 8 --hidden_size 100 B
+python deeplog_demo.py --dataset HDFS --feature_type sequentials --train_anomaly_ratio 0 --label_type next_log --gpu 0 --embedding_dim 8 --hidden_size 200 B
+python deeplog_demo.py --dataset HDFS --feature_type sequentials --train_anomaly_ratio 0 --label_type next_log --gpu 0 --embedding_dim 8 --hidden_size 300 B
+python deeplog_demo.py --dataset HDFS --feature_type sequentials --train_anomaly_ratio 0 --label_type next_log --gpu 0 --embedding_dim 8 --hidden_size 400 B
+python deeplog_demo.py --dataset HDFS --feature_type sequentials --train_anomaly_ratio 0 --label_type next_log --gpu 0 --embedding_dim 8 --hidden_size 500 B
