29781bf07df886db7ac1178fd55cfcccc86cba35
ase21-843 <83080529+ase21-843@users.noreply.github.com>
2021-05-26 19:59:46 +0800

    update

--- demo/deeplog_demo.py
+++ demo/deeplog_demo.py
@@ -43,9 +43,9 @@ parser.add_argument("--dataset", default="BGL", type=str)
 parser.add_argument("--train_anomaly_ratio", default=0, type=float)
 parser.add_argument("--random_partition", action="store_true")
 parser.add_argument("--train_ratio", default=None, type=float)
-parser.add_argument("--test_ratio", default=0.5, type=float)
+parser.add_argument("--test_ratio", default=0.2, type=float)
 parser.add_argument("--window_size", default=3, type=int)
-parser.add_argument("--stride", default=2, type=int)
+parser.add_argument("--stride", default=1, type=int)
 
 ##### training params
 parser.add_argument("--epoches", default=5, type=int)
