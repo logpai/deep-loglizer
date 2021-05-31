99c7e16a683815b0e55ed899f449ff61e618abb0
ase21-843 <83080529+ase21-843@users.noreply.github.com>
2021-05-31 17:57:06 +0800

    update

---
+++ demo/runs/HDFS/w_semantics/cnn.sh
@@ -0,0 +1,10 @@
+# --hidden_size 200
+# --num_directions 1
+# --embedding_dim 8
+# --hidden_size 200
+# --kernel_sizes 2 3 4
+# --embedding_dim 8
+
+# wo semantics
+python cnn_demo.py --dataset HDFS --pkl_dir ../data/processed/HDFS/hdfs_no_train_anomaly_8_2 --window_size 10 --feature_type semantics --use_tfidf
+
