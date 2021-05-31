df8318f8c0457a6ae3dbaa0e27f8a0ddb4212f71
ase21-843 <83080529+ase21-843@users.noreply.github.com>
2021-05-31 16:16:15 +0800

    update

--- demo/ae_demo.py
+++ demo/ae_demo.py
@@ -44,8 +44,8 @@ parser.add_argument("--window_size", default=10, type=int)
 parser.add_argument("--stride", default=1, type=int)
 
 ##### training params
-parser.add_argument("--epoches", default=5, type=int)
-parser.add_argument("--learning_rate", default=0.01, type=float)
+parser.add_argument("--epoches", default=10, type=int)
+parser.add_argument("--learning_rate", default=0.001, type=float)
 parser.add_argument("--batch_size", default=1024, type=int)
 parser.add_argument("--anomaly_ratio", default=0.03, type=float)
 # 0.03 for HDFS, xxx for BGL
