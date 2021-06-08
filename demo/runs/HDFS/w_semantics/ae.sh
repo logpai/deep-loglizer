72bd8c3868dc47874d0b804c857c22012d9d0c56
ase21-843 <83080529+ase21-843@users.noreply.github.com>
2021-06-08 11:17:31 +0800

    update

--- demo/runs/HDFS/w_semantics/ae.sh
+++ demo/runs/HDFS/w_semantics/ae.sh
@@ -6,5 +6,5 @@
 
 
 # wo semantics
-python ae_demo.py --dataset HDFS --pkl_dir ../data/processed/HDFS/hdfs_no_train_anomaly_8_2 --window_size 10 --feature_type semantics --use_tfidf B
+python ae_demo.py --dataset HDFS --pkl_dir ../data/processed/HDFS/hdfs_no_train_anomaly_8_2 --window_size 10 --anomaly_ratio 0.03  --feature_type semantics --use_tfidf B
 
--- demo/runs/HDFS/w_semantics/transformer.sh
+++ demo/runs/HDFS/w_semantics/transformer.sh
@@ -6,3 +6,24 @@
 # wo semantics
 python transformer_demo.py --dataset HDFS --pkl_dir ../data/processed/HDFS/hdfs_no_train_anomaly_8_2 --window_size 10 --feature_type semantics --use_tfidf B
 
+python transformer_demo.py --dataset HDFS --pkl_dir ../data/processed/HDFS/hdfs_no_train_anomaly_8_2 --window_size 10 --feature_type semantics --use_tfidf --embedding_dim 4 --nhead 1  --hidden_size 100 --gpu 0 B
+
+python transformer_demo.py --dataset HDFS --pkl_dir ../data/processed/HDFS/hdfs_no_train_anomaly_8_2 --window_size 10 --feature_type semantics --use_tfidf --embedding_dim 8 --nhead 1  --hidden_size 100 --gpu 0 B
+
+python transformer_demo.py --dataset HDFS --pkl_dir ../data/processed/HDFS/hdfs_no_train_anomaly_8_2 --window_size 10 --feature_type semantics --use_tfidf --embedding_dim 16 --nhead 1  --hidden_size 100 --gpu 0 B
+
+python transformer_demo.py --dataset HDFS --pkl_dir ../data/processed/HDFS/hdfs_no_train_anomaly_8_2 --window_size 10 --feature_type semantics --use_tfidf --embedding_dim 4 --nhead 2  --hidden_size 100 --gpu 1 B
+
+python transformer_demo.py --dataset HDFS --pkl_dir ../data/processed/HDFS/hdfs_no_train_anomaly_8_2 --window_size 10 --feature_type semantics --use_tfidf --embedding_dim 4 --nhead 3  --hidden_size 100 --gpu 1 B
+
+python transformer_demo.py --dataset HDFS --pkl_dir ../data/processed/HDFS/hdfs_no_train_anomaly_8_2 --window_size 10 --feature_type semantics --use_tfidf --embedding_dim 4 --nhead 1  --hidden_size 32 --gpu 1 B
+
+python transformer_demo.py --dataset HDFS --pkl_dir ../data/processed/HDFS/hdfs_no_train_anomaly_8_2 --window_size 10 --feature_type semantics --use_tfidf --embedding_dim 4 --nhead 1  --hidden_size 200 --gpu 1 B
+
+python transformer_demo.py --dataset HDFS --pkl_dir ../data/processed/HDFS/hdfs_no_train_anomaly_8_2 --window_size 10 --feature_type semantics --use_tfidf --embedding_dim 4 --nhead 1  --hidden_size 300 --gpu 0 B
+
+
+
+
+
+
