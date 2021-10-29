64a786d58eca2aebce5c9b0ea4b83d52c60adff1
jinyang88 <liujy725@gmail.com>
2021-10-29 11:07:48 +0800

    complete requirements file

--- deeploglizer/common/utils.py
+++ deeploglizer/common/utils.py
@@ -3,7 +3,6 @@ import torch
 import random
 import os
 import numpy as np
-import h5py
 import json
 import pickle
 import random
@@ -118,46 +117,3 @@ def load_pickle(file_path):
     with open(file_path, "rb") as fr:
         return pickle.load(fr)
 
-
-def save_dict_to_hdf5(dic, filename):
-    """
-    ....
-    """
-    with h5py.File(filename, "w") as h5file:
-        recursively_save_dict_contents_to_group(h5file, "/", dic)
-
-
-def recursively_save_dict_contents_to_group(h5file, path, dic):
-    """
-    ....
-    """
-    for key, item in dic.items():
-        if isinstance(item, (np.ndarray, np.int64, np.float64, str, bytes)):
-            h5file[path + key] = item
-        elif isinstance(item, dict):
-            recursively_save_dict_contents_to_group(h5file, path + key + "/", item)
-        else:
-            raise ValueError("Cannot save %s type" % type(item))
-
-
-def load_dict_from_hdf5(filename):
-    """
-    ....
-    """
-    with h5py.File(filename, "r") as h5file:
-        return recursively_load_dict_contents_from_group(h5file, "/")
-
-
-def recursively_load_dict_contents_from_group(h5file, path):
-    """
-    ....
-    """
-    ans = {}
-    for key, item in h5file[path].items():
-        if isinstance(item, h5py._hl.dataset.Dataset):
-            ans[key] = item.value
-        elif isinstance(item, h5py._hl.group.Group):
-            ans[key] = recursively_load_dict_contents_from_group(
-                h5file, path + key + "/"
-            )
-    return ans
